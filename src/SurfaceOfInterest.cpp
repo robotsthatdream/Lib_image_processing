1bc6f99f4c9e56e88b2ae13ebb04e874791a6614
Le Goff <le_goff@isir.upmc.fr>
2016-06-28 11:52:36 +0200

    merge

--- src/SurfaceOfInterest.cpp
+++ src/SurfaceOfInterest.cpp
@@ -201,6 +201,8 @@ void SurfaceOfInterest::compute_confidence_weights(oml::Classifier::ConstPtr mod
         _weights[itr->first] = (s.w - .5)*2.;
         if(_weights[itr->first] > 1)
             _weights[itr->first] = 1;
+
+        _weights[itr->first] = 1 - _weights[itr->first];
     }
 
 }
