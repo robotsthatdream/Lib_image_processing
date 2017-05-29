0adc8980aacaff525dd254b94cf5d4f0bec8b757
Jonathan CROUZET <jonathan.crouzet.jc@gmail.com>
2017-05-29 15:10:03 +0200

    added a threshold to delete objects with few points in get_objects

--- src/SurfaceOfInterest.cpp
+++ src/SurfaceOfInterest.cpp
@@ -306,7 +306,8 @@ pcl::PointCloud<pcl::PointXYZI> SurfaceOfInterest::cumulative_relevance_map(std:
 }
 
 
-std::vector<Object> SurfaceOfInterest::get_objects(const std::string &modality, double &saliency_threshold){
+
+std::vector<Object> SurfaceOfInterest::get_objects(const std::string &modality, double &saliency_threshold, int &points_threshold){
     std::vector<Object> objs;
 
     std::map<pcl::Supervoxel<PointT>::Ptr, int> clusters = get_supervoxels_clusters(modality, saliency_threshold);
@@ -319,7 +320,7 @@ std::vector<Object> SurfaceOfInterest::get_objects(const std::string &modality, 
             *current_cloud += *(it->first->voxels_);
         }
         else {
-            if (current_id != -1) {
+            if (current_id != -1 && current_cloud->size() > points_threshold) {
                 Object obj(*current_cloud);
                 objs.push_back(obj);
             }
