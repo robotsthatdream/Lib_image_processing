8cf3ab9dc9daecfc328fb064a6eb6930448aae81
Jonathan CROUZET <jonathan.crouzet.jc@gmail.com>
2017-06-13 14:50:16 +0200

    fixed feedback's pointcloud generation

--- src/Objects.cpp
+++ src/Objects.cpp
@@ -5,11 +5,11 @@
 
 using namespace image_processing;
 
-void Object::update_cloud(PointCloudT current_cloud)
-{
-  // extract features comparing current and previous cloud
-  // std::vector<double> feature;
-  // features.push_back(feature);
-  // update object cloud
-  object_cloud = current_cloud;
-}
+// void Object::update_cloud(PointCloudT current_cloud)
+// {
+//   // extract features comparing current and previous cloud
+//   // std::vector<double> feature;
+//   // features.push_back(feature);
+//   // update object cloud
+//   object_cloud = current_cloud;
+// }
