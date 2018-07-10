2247a135142321327902f9b3596871b0fe6f98d0
leni le goff <lenilegoff@gmail.com>
2018-07-10 18:51:55 +0200

    add new test to generate objects hypothesis from relevance map this add dependancy for compilation to iagmm library

---
+++ test/test_object_hyp.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include "../include/image_processing/SurfaceOfInterest.h"
+#include <iagmm/gmm.hpp>
+#include <boost/archive/text_iarchive.hpp>
+
+namespace ip = image_processing;
+
+int main(int argc, char** argv){
+
+    if(argc != 3){
+        std::cerr << "Usage : \n\t- pcd file\n\t- gmm archive" << std::endl;
+        return 1;
+    }
+
+    std::string pcd_file = argv[1];
+    std::string gmm_archive = argv[2];
+
+    //* Load pcd file into a pointcloud
+    ip::PointCloudT::Ptr input_cloud(new ip::PointCloudT);
+    pcl::io::loadPCDFile(pcd_file, *input_cloud);
+    //*/
+
+    std::cout << "pcd file loaded" << std::endl;
+
+    //* Load the CMMs classifier from the archive
+    std::ifstream ifs(gmm_archive);
+    if(!ifs){
+        std::cerr << "Unable to open archive : " << gmm_archive << std::endl;
+        return 1;
+    }
+    iagmm::GMM gmm;
+    boost::archive::text_iarchive iarch(ifs);
+    iarch >> gmm;
+    //*/
+
+    std::cout << "classifier archive loaded" << std::endl;
+
+    //* Generate relevance map on the pointcloud
+    ip::SurfaceOfInterest soi(input_cloud);
+    soi.computeSupervoxel();
+    soi.compute_feature("meanFPFHLabHist");
+    soi.compute_weights<iagmm::GMM>("meanFPFHLabHist",gmm);
+    //*/
+
+    std::cout << "relevance_map extracted" << std::endl;
+
+    //* Generate objects hypothesis
+    std::vector<std::set<uint32_t>> obj_indexes;
+    obj_indexes = soi.extract_regions("meanFPFHLabHist",0.5,1);
+    //*/
+
+    std::cout << obj_indexes.size() <<  " objects hypothesis extracted" << std::endl;
+
+    return 0;
+}
