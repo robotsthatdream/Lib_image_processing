fb9397fec44f235508d8366ab8743e78f88c8bd1
Stéphane Gourichon <stephane.gourichon@isir.upmc.fr>
2019-01-30 15:23:24 +0100

    Fix args order

--- any_command_add_j_automatic_parallel_jobs_count.sh
+++ any_command_add_j_automatic_parallel_jobs_count.sh
@@ -29,5 +29,5 @@ MY_EXTRA_ARGS="-j$MAX_PARALLEL_JOBS_REQUESTED"
     echo "Args: $@"
 } | tee -a compilation_memory_use.log >&2
 
-exec /usr/bin/time --format='%M' --append --output=compilation_memory_use.log "$MY_EXTRA_ARGS" "$@"
+exec /usr/bin/time --format='%M' --append --output=compilation_memory_use.log "$@" "$MY_EXTRA_ARGS"
 #-k ${BUILD_MAX_PARALLEL_PROCESSES:-${PROCESSOR_COUNT}} "$@"
