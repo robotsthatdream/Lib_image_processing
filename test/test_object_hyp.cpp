e020496e030dc2c48d9cbf744a277d0b61ac9d30
Stéphane Gourichon <stephane.gourichon@isir.upmc.fr>
2019-06-07 10:49:10 +0200

    Use log(x) rather than x-1 as a measure of deviation from sphere.

--- test/test_object_hyp.cpp
+++ test/test_object_hyp.cpp
@@ -337,7 +337,7 @@ struct OptimizationFunctor : pcl::Functor<float> {
                 powf_abs(v_scaled(2), two_over_exp_1);
             // FSG_LOG_VAR(outside_if_over_1);
 
-            const float deviation = (outside_if_over_1 - 1);
+            const float deviation = log(outside_if_over_1);
             // FSG_LOG_VAR(deviation);
 
             fvec[i] = deviation;
